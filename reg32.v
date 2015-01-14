907ef5432181782f8ed6bbf4db80823e8f8cc948
ChanPhilip77 <Zahl506@gmail.com>
2015-01-13 17:26:12 -0800

    Update reg32.v

--- reg32.v
+++ reg32.v
@@ -1,5 +1,6 @@
-module reg32 (clock, resetn, D, byteenable, Q)
-input [0:0] clock, resetn;
+module reg32 (clock, resetn, D, byteenable, Q);
+input [0:0] clock;
+input [0:0] resetn;
 input [31:0] D;
 input [1:0] byteenable;
 output reg [31:0] Q;
@@ -7,7 +8,8 @@ output reg [31:0] Q;
 always @(posedge clock or negedge resetn) begin
 	if (resetn == 0) begin
 		Q = 0;
-	else
+	end
+	else begin
 		Q = D;
 	end // if (reset == 1)
 	
