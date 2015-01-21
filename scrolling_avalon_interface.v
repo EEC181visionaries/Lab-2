b5affe2c72e6e7bdc0741ef983a0d43e8252af40
MaJessica9 <jamma@ucdavis.edu>
2015-01-20 16:39:53 -0800

    Update scrolling_avalon_interface.v

--- scrolling_avalon_interface.v
+++ scrolling_avalon_interface.v
@@ -1,7 +1,7 @@
-module scrolling_avalon_interface (clock, write, writedata, read, readdata, byteenable, chipselect, out);
+module scrolling_avalon_interface (clock, resetn, write, writedata, read, readdata, byteenable, chipselect, val_out);
 
 	// Standard clock and reset signals
-	input clock;
+	input clock, resetn;
 
 	// Avalon MMI signals - These will be automatically be detected and organized by Qsys
 	input read, write, chipselect;
@@ -10,7 +10,7 @@ module scrolling_avalon_interface (clock, write, writedata, read, readdata, byte
 	output [31:0] readdata;
 
 	// Signal for exporting register contents to the outside of the embedded system
-	output [31:0] out;
+	output [31:0] val_out;
 
 	// Internal signals used in this module only
 	wire [3:0] local_byteenable;
@@ -19,9 +19,9 @@ module scrolling_avalon_interface (clock, write, writedata, read, readdata, byte
 	assign to_reg = writedata;
 	assign local_byteenable = (chipselect & write) ? byteenable : 4'b0;
 	assign readdata = from_reg;
-	assign out = from_reg;
+	assign val_out = from_reg;
 
-	scrolling U1 (.clock(clock), .speed_in(to_reg), .byteenable(local_byteenable), .out(from_reg));
+	scrolling U1 (.clock(clock), .resetn(resetn), .byteenable(local_byteenable), .speed_in(to_reg), .val_out(from_reg));
 
 
 endmodule
